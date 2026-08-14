c2645ff567498b3141598f0173a5306383bfa0d4
katalinalyn <katalinalyn11@gmail.com>
2026-08-14 09:59:08 +0800

    content_rating

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,13 +5,46 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
-  }
+    this.state = {
+      like: 0,
+      dislike: 0,
+      handleLike: () => {
+        this.setState((prevState)  => ({
+          like: prevState.like + 1,
+        }));
+
+      },
+
+      handledislike: () => {
+        this.setState((prevState)  => ({
+          dislike: prevState.dislike + 1,
+        }));
+    },
+    
+  };
+}
   render() {
     return (
+
      <>
-     <h1>Text Content Rating</h1>
-     </>
+    <div className="content-rating">
+        <h1>Text Content Rating</h1> 
+        <p>"Doubt kills more dreams than failure ever will."</p>
+  
+
+      <div className="rating-button"> 
+          <button className="like-button" onClick={() => this.setState({ like: this.state.like + 1 })}>  
+          Like ({this.state.like})
+        </button>
+         <button className="dislike-button" onClick={() => this.setState({ dislike: this.state.dislike + 1 })}>  
+          dislike ({this.state.dislike})
+        </button>
+    </div>
+    </div>
+  </>
+     
     );
+
   }
 }
 
